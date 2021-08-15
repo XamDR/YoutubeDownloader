627115a926b6abf609d5e8c664422a5eb12e9b6b
XamDR <maxdr.mat@gmail.com>
2021-08-14 23:07:52 -0500

    Added auto select text on focus behavior for the TextBox that is used to enter the Youtube urls.

---
+++ YoutubeDownloader/Behaviors/AutoSelectTextOnFocusBehavior.cs
@@ -0,0 +1,39 @@
+using System.Windows.Controls;
+using System.Windows.Input;
+using Microsoft.Xaml.Behaviors;
+
+namespace YoutubeDownloader.Behaviors
+{
+	public class AutoSelectTextOnFocusBehavior : Behavior<TextBox>
+	{
+		protected override void OnAttached()
+		{
+			base.OnAttached();
+			AssociatedObject.GotKeyboardFocus += AssociatedObjectGotKeyboardFocus;
+			AssociatedObject.GotMouseCapture += AssociatedObjectGotMouseCapture;
+			AssociatedObject.PreviewMouseLeftButtonDown += AssociatedObjectPreviewMouseLeftButtonDown;
+		}
+
+		protected override void OnDetaching()
+		{
+			base.OnDetaching();
+			AssociatedObject.GotKeyboardFocus -= AssociatedObjectGotKeyboardFocus;
+			AssociatedObject.GotMouseCapture -= AssociatedObjectGotMouseCapture;
+			AssociatedObject.PreviewMouseLeftButtonDown -= AssociatedObjectPreviewMouseLeftButtonDown;
+		}
+		
+		private void AssociatedObjectGotKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e) 
+			=> AssociatedObject.SelectAll();
+
+		private void AssociatedObjectGotMouseCapture(object sender, MouseEventArgs e) => AssociatedObject.SelectAll();
+
+		private void AssociatedObjectPreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+		{
+			if(!AssociatedObject.IsKeyboardFocusWithin)
+			{
+				AssociatedObject.Focus();
+				e.Handled = true;
+			}
+		}
+	}
+}
